fa483e1df79238580830444ad80d15d69ec9e690
Patryk Szwermer <patryk.szwer@gmail.com>
2018-07-12 14:05:06 +0200

    Moved UniDataTableTests.cs file to proper folder

--- Starcounter.Uniform.Tests/ViewModels/UniDataTableTests.cs
+++ Starcounter.Uniform.Tests/ViewModels/UniDataTableTests.cs
@@ -8,7 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Starcounter.Uniform.Tests.Builder
+namespace Starcounter.Uniform.Tests.ViewModels
 {
     public class UniDataTableTests
     {
